782fb0301b9fc8e2c98a56918e208ec557fa0672
lortas <lortas@freenet.de>
2011-09-06 21:53:18 +0000

    Werkzeug zum zusammen führen von capabilities.xml's fertiggestellt

--- src/de/earthdawn/tool/JoinCapabilities.java
+++ src/de/earthdawn/tool/JoinCapabilities.java
@@ -1,6 +1,4 @@
 package de.earthdawn.tool;
-
-
 /******************************************************************************\
 Copyright (C) 2010-2011  Holger von Rhein <lortas@freenet.de>
 
@@ -26,12 +24,15 @@ import java.io.IOException;
 import java.io.PrintStream;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBElement;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
 import javax.xml.bind.Marshaller;
+import javax.xml.namespace.QName;
 
 import de.earthdawn.config.ECECapabilities;
 import de.earthdawn.data.CAPABILITIES;
@@ -40,11 +41,11 @@ import de.earthdawn.data.CAPABILITYType;
 public class JoinCapabilities {
 	private static final String encoding="UTF-8";
 
-	public static int main(String[] args) {
+	public static void main(String[] args) {
 		if( args.length < 2 ) {
-			System.out.println("Zuwenige Paramter");
-			System.out.println("JoinCapabilities infile1 [infile2 ...] outfile");
-			return -1;
+			System.out.println("not engouth paramter.");
+			System.out.println("use JoinCapabilities infile1 [infile2 ...] outfile");
+			return;
 		}
 		try {
 			JAXBContext jc = JAXBContext.newInstance("de.earthdawn.data");
@@ -52,20 +53,39 @@ public class JoinCapabilities {
 			int countInFiles=args.length-1;
 			List<CAPABILITYType> talents = new ArrayList<CAPABILITYType>();
 			List<CAPABILITYType> skills = new ArrayList<CAPABILITYType>();
+			String language=null;
 			for( int i=0; i<countInFiles; i++ ) {
-				ECECapabilities caps = new ECECapabilities(((CAPABILITIES) u.unmarshal(new File(args[i]))).getSKILLOrTALENT());
+				CAPABILITIES capabilities = (CAPABILITIES) u.unmarshal(new File(args[i]));
+				ECECapabilities caps = new ECECapabilities(capabilities.getSKILLOrTALENT());
+				if( language == null ) language = capabilities.getLang();
+				else if( ! capabilities.getLang().equals(language) ) System.err.println("Languages are not identical: '"+language+"'!='"+capabilities.getLang()+"'");
 				talents.addAll(caps.getTalents());
 				skills.addAll(caps.getSkills());
 			}
 			CAPABILITIES outCapabilities = new CAPABILITIES();
+			outCapabilities.setLang(language);
+			Collections.sort(talents, new CapabilityComparator());
+			Collections.sort(skills, new CapabilityComparator());
+			List<JAXBElement<CAPABILITYType>> skillOrTalent = outCapabilities.getSKILLOrTALENT();
+			for( CAPABILITYType skill : skills ) {
+				QName qName = new QName("http://earthdawn.com/capability","SKILL");
+				JAXBElement<CAPABILITYType> jaxb = new JAXBElement<CAPABILITYType>(qName, CAPABILITYType.class, skill);
+				skillOrTalent.add(jaxb);
+			}
+			for( CAPABILITYType talent : talents ) {
+				QName qName = new QName("http://earthdawn.com/capability","TALENT");
+				JAXBElement<CAPABILITYType> jaxb = new JAXBElement<CAPABILITYType>(qName, CAPABILITYType.class, talent);
+				skillOrTalent.add(jaxb);
+			}
 			Marshaller m = jc.createMarshaller();
+			// Das letzte Element ist die Zieldatei, in der die Ausgabe hinein geschrieben wird.
 			FileOutputStream out = new FileOutputStream(args[countInFiles]);
 			PrintStream fileio = new PrintStream(out, false, encoding);
 			m.setProperty(Marshaller.JAXB_ENCODING, encoding);
 			m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
 			m.setProperty(Marshaller.JAXB_SCHEMA_LOCATION,"http://earthdawn.com/capability earthdawncapabilities.xsd");
 			m.setProperty(Marshaller.JAXB_FRAGMENT, true);
-			fileio.print("<?xml version=\"1.0\" encoding=\""+encoding+"\" standalone=\"no\"?>\n<?xml-stylesheet type=\"text/xsl\" href=\"earthdawncharacter.xsl\"?>");
+			fileio.print("<?xml version=\"1.0\" encoding=\""+encoding+"\" standalone=\"no\"?>");
 			m.marshal(outCapabilities,fileio);
 			fileio.close();
 			out.close();
@@ -78,6 +98,5 @@ public class JoinCapabilities {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		return 0;
 	}
 }
