9f290d6363ea847fcdd71a686a24bf4bc2b2c842
lortas <lortas@freenet.de>
2011-10-20 05:14:16 +0000

    characteristic layout verändert

--- src/de/earthdawn/ui2/tree/BloodcharmNodePanel.java
+++ src/de/earthdawn/ui2/tree/BloodcharmNodePanel.java
@@ -1,14 +1,12 @@
 package de.earthdawn.ui2.tree;
 
 import de.earthdawn.data.BLOODCHARMITEMType;
-import de.earthdawn.data.ITEMType;
 import de.earthdawn.data.YesnoType;
 import de.earthdawn.data.ItemkindType;
 
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.JSpinner;
-import javax.swing.JList;
 import javax.swing.JCheckBox;
 import javax.swing.SpinnerNumberModel;
 import javax.swing.JComboBox;
@@ -41,9 +39,10 @@ public class BloodcharmNodePanel extends AbstractNodePanel<BLOODCHARMITEMType> {
 
 	public BloodcharmNodePanel(BLOODCHARMITEMType node) {
 		super(node);
-		this.setPreferredSize(new Dimension(710, 65));
+		this.setPreferredSize(new Dimension(770, 91));
+		//this.setOpaque(true);
 		setBorder(new LineBorder(new Color(0, 0, 0)));
-		setLayout(new MigLayout("", "[24px][128px][27px][86px,grow][34px][47px][40px][86px][49px]", "[23px][]"));
+		setLayout(new MigLayout("", "[24px][128px][79.00px][:185.00px:48.00px,grow][34px][47px][40px][86px][49px]", "[23px][][]"));
 		
 		lblType = new JLabel("Type");
 		add(lblType, "cell 0 0,alignx left,aligny center");
@@ -53,13 +52,8 @@ public class BloodcharmNodePanel extends AbstractNodePanel<BLOODCHARMITEMType> {
 		add(comboBoxType, "cell 1 0,alignx left,aligny center");
 		comboBoxType.setSelectedItem(nodeObject.getKind());
 		
-		lblName = new JLabel("Name");
-		add(lblName, "cell 2 0,alignx left,aligny center");
-		
-		textFieldName = new JTextField();
-		add(textFieldName, "cell 3 0,alignx left,aligny center");
-		textFieldName.setColumns(10);
-		textFieldName.setText(nodeObject.getName());
+		lblBlooddamage = new JLabel("Blooddamage");
+		add(lblBlooddamage, "cell 2 0");
 		
 		lblWeight = new JLabel("Weight");
 		add(lblWeight, "cell 4 0,alignx left,aligny center");
@@ -76,35 +70,40 @@ public class BloodcharmNodePanel extends AbstractNodePanel<BLOODCHARMITEMType> {
 		textFieldLocation.setColumns(10);
 		textFieldLocation.setText(node.getLocation());
 		
-		chckbxUsed = new JCheckBox("Used");
-		chckbxUsed.setBackground(Color.WHITE);
-		add(chckbxUsed, "cell 8 0,alignx left,aligny top");
-		chckbxUsed.setSelected(node.getUsed() == YesnoType.YES);
-		
-		lblBlooddamage = new JLabel("Blooddamage");
-		add(lblBlooddamage, "cell 0 1");
-		
-		spinnerBloodDamage = new JSpinner();
-		add(spinnerBloodDamage, "cell 1 1");
-		
-		lblEffect = new JLabel("Effect");
-		add(lblEffect, "cell 2 1,alignx trailing");
-		
-		textFieldEffect = new JTextField();
-		add(textFieldEffect, "cell 3 1,growx");
-		textFieldEffect.setColumns(10);
+		textFieldName = new JTextField();
+		add(textFieldName, "cell 1 1,alignx left,aligny center");
+		textFieldName.setColumns(10);
+		textFieldName.setText(nodeObject.getName());
 		
 		lblDepatterningrate = new JLabel("Depatterningrate");
-		add(lblDepatterningrate, "cell 4 1");
+		add(lblDepatterningrate, "cell 2 1");
 		
 		spinnerDepatterningrate = new JSpinner();
-		add(spinnerDepatterningrate, "cell 5 1");
+		add(spinnerDepatterningrate, "cell 3 1");
 		
 		lblEnchantingDn = new JLabel("Enchanting DN");
 		add(lblEnchantingDn, "cell 6 1");
 		
 		spinnerEnchantingdifficultynumber = new JSpinner();
 		add(spinnerEnchantingdifficultynumber, "cell 7 1");
+		
+		spinnerBloodDamage = new JSpinner();
+		add(spinnerBloodDamage, "cell 3 0");
+		
+		lblName = new JLabel("Name");
+		add(lblName, "cell 0 1,alignx left,aligny center");
+		
+		lblEffect = new JLabel("Effect");
+		add(lblEffect, "cell 0 2,alignx trailing");
+		
+		textFieldEffect = new JTextField();
+		add(textFieldEffect, "cell 1 2 7 1,growx");
+		textFieldEffect.setColumns(10);
+		
+		chckbxUsed = new JCheckBox("Used");
+		chckbxUsed.setBackground(Color.WHITE);
+		add(chckbxUsed, "cell 8 2,alignx left,aligny top");
+		chckbxUsed.setSelected(node.getUsed() == YesnoType.YES);
 	}
 
 	@Override
