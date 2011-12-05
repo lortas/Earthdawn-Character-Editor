cdcbf0b0db2df3b94f1cd82be852cbbb0fa6e4b9
Holger <lortas@freenet.de>
2011-12-05 11:38:52 +0000

    Die Attribut-Tabelle um die Spalte 'Dice' erweitert

--- src/de/earthdawn/ui2/EDAttributes.java
+++ src/de/earthdawn/ui2/EDAttributes.java
@@ -19,15 +19,10 @@ import de.earthdawn.ECEWorker;
 import de.earthdawn.config.ApplicationProperties;
 
 public class EDAttributes extends JPanel {
-	
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = -6319723413147452999L;
 	private CharacterContainer character;
 	public static final ApplicationProperties PROPERTIES=ApplicationProperties.create();
 	public static final int lpincrease_max = PROPERTIES.getOptionalRules().getATTRIBUTE().getLimitlpincrease();
-
 	private JScrollPane scrollPane;
 	private JTable table;
 	private BufferedImage backgroundimage = null;
@@ -90,19 +85,16 @@ class AttributesTableModel extends AbstractTableModel {
 	
 	private CharacterContainer character;
 	
-    private String[] columnNames = {"Attribute", "Base", "Buy", "LP increase", "Final", "Step"};
-    private Object[][] data = {
-    {"Dexterity"	, new Integer(0), new Integer(0), new Integer(0), new Integer(0), new Integer(0)},
-    {"Strength"		, new Integer(0), new Integer(0), new Integer(0), new Integer(0), new Integer(0)},    
-    {"Toughness"	, new Integer(0), new Integer(0), new Integer(0), new Integer(0), new Integer(0)},
-    {"Perception"	, new Integer(0), new Integer(0), new Integer(0), new Integer(0), new Integer(0)},
-    {"Willpower"	, new Integer(0), new Integer(0), new Integer(0), new Integer(0), new Integer(0)},
-    {"Charisma"		, new Integer(0), new Integer(0), new Integer(0), new Integer(0), new Integer(0)},
-    };
-    
-    private String[] columnKey = {"DEX", "STR", "TOU", "PER", "WIL", "CHA"};
-    
-  
+	private String[] columnNames = {"Attribute", "Base", "Buy", "LP increase", "Final", "Step", "Dice"};
+	private Object[][] data = {
+	{"Dexterity"	, new Integer(0), new Integer(0), new Integer(0), new Integer(0), new Integer(0), ""},
+	{"Strength"		, new Integer(0), new Integer(0), new Integer(0), new Integer(0), new Integer(0), ""},
+	{"Toughness"	, new Integer(0), new Integer(0), new Integer(0), new Integer(0), new Integer(0), ""},
+	{"Perception"	, new Integer(0), new Integer(0), new Integer(0), new Integer(0), new Integer(0), ""},
+	{"Willpower"	, new Integer(0), new Integer(0), new Integer(0), new Integer(0), new Integer(0), ""},
+	{"Charisma"		, new Integer(0), new Integer(0), new Integer(0), new Integer(0), new Integer(0), ""},
+	};
+	private String[] columnKey = {"DEX", "STR", "TOU", "PER", "WIL", "CHA"};
 	public void setCharacter(CharacterContainer character) {
 		this.character = character;
 	}
@@ -111,81 +103,67 @@ class AttributesTableModel extends AbstractTableModel {
 		return character;
 	}	
 
-    public int getColumnCount() {
-        return columnNames.length;
-    }
+	public int getColumnCount() {
+		return columnNames.length;
+	}
 
-    public int getRowCount() {
-        return data.length;
-    }
+	public int getRowCount() {
+		return data.length;
+	}
 
-    public String getColumnName(int col) {
-        return columnNames[col];
-    }
+	public String getColumnName(int col) {
+		return columnNames[col];
+	}
 
-    public Object getValueAt(int row, int col) {
-    	if (character == null){
-    		return data[row][col];
-    	}
-    		
-    
-        switch (col) {
-	        case 0:  return data[row][col]; 
-	        case 1:  return new Integer(character.getAttributes().get(columnKey[row]).getRacevalue());
-	        case 2:  return new Integer(character.getAttributes().get(columnKey[row]).getGenerationvalue());
-	        case 3:  return new Integer(character.getAttributes().get(columnKey[row]).getLpincrease());
-	        case 4:  return new Integer(character.getAttributes().get(columnKey[row]).getCurrentvalue());
-	        case 5:  return new Integer(character.getAttributes().get(columnKey[row]).getStep());
-	        default: return new Integer(0);
-        }
-
-
-    }
-
-    /*
-     * JTable uses this method to determine the default renderer/
-     * editor for each cell.  If we didn't implement this method,
-     * then the last column would contain text ("true"/"false"),
-     * rather than a check box.
-     */
-    public Class getColumnClass(int c) {
-        return getValueAt(0, c).getClass();
-    }
-
-    /*
-     * Don't need to implement this method unless your table's
-     * editable.
-     */
-    public boolean isCellEditable(int row, int col) {
-        //Note that the data/cell address is constant,
-        //no matter where the cell appears onscreen.
-        if ((col == 2) || (col == 3)) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    /*
-     * Don't need to implement this method unless your table's
-     * data can change.
-     */
-    public void setValueAt(Object value, int row, int col) {
-        data[row][col] = value;
-        
-        switch (col) {
-	        case 2:  character.getAttributes().get(columnKey[row]).setGenerationvalue((Integer)value); break;
-	        case 3:  character.getAttributes().get(columnKey[row]).setLpincrease((Integer)value); break;
-	        default: break;
-        }
-        ECEWorker worker = new ECEWorker();
-        worker.verarbeiteCharakter(character.getEDCHARACTER());
-        character.refesh();
-        fireTableCellUpdated(row, col);
-        fireTableCellUpdated(row, 4);
-        fireTableCellUpdated(row, 5);
-    }
-
+	public Object getValueAt(int row, int col) {
+		if (character == null){
+			return data[row][col];
+		}
+		switch (col) {
+		case 0:  return data[row][col]; 
+		case 1:  return new Integer(character.getAttributes().get(columnKey[row]).getRacevalue());
+		case 2:  return new Integer(character.getAttributes().get(columnKey[row]).getGenerationvalue());
+		case 3:  return new Integer(character.getAttributes().get(columnKey[row]).getLpincrease());
+		case 4:  return new Integer(character.getAttributes().get(columnKey[row]).getCurrentvalue());
+		case 5:  return new Integer(character.getAttributes().get(columnKey[row]).getStep());
+		case 6:  return character.getAttributes().get(columnKey[row]).getDice().value();
+		default: return new Integer(0);
+	}
 }
 
+	/*
+	 * JTable uses this method to determine the default renderer/
+	 * editor for each cell.  If we didn't implement this method,
+	 * then the last column would contain text ("true"/"false"),
+	 * rather than a check box.
+	 */
+	public Class getColumnClass(int c) {
+		return getValueAt(0, c).getClass();
+	}
 
+	public boolean isCellEditable(int row, int col) {
+		//Note that the data/cell address is constant,
+		//no matter where the cell appears onscreen.
+		if ((col == 2) || (col == 3)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public void setValueAt(Object value, int row, int col) {
+		data[row][col] = value;
+		switch (col) {
+			case 2:  character.getAttributes().get(columnKey[row]).setGenerationvalue((Integer)value); break;
+			case 3:  character.getAttributes().get(columnKey[row]).setLpincrease((Integer)value); break;
+			default: break;
+		}
+		ECEWorker worker = new ECEWorker();
+		worker.verarbeiteCharakter(character.getEDCHARACTER());
+		character.refesh();
+		fireTableCellUpdated(row, col);
+		fireTableCellUpdated(row, 4);
+		fireTableCellUpdated(row, 5);
+		fireTableCellUpdated(row, 6);
+	}
+}
