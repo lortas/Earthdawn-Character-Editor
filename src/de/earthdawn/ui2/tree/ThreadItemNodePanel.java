fc2171ade1661b6b800e18b318187149c1743e50
lortas <lortas@freenet.de>
2011-12-25 23:28:02 +0000

    Layout vom ThreadItemNodePanel komplett überarbeitet

--- src/de/earthdawn/ui2/tree/ThreadItemNodePanel.java
+++ src/de/earthdawn/ui2/tree/ThreadItemNodePanel.java
@@ -1,6 +1,5 @@
 package de.earthdawn.ui2.tree;
 
-import de.earthdawn.data.ITEMType;
 import de.earthdawn.data.THREADITEMType;
 import de.earthdawn.data.YesnoType;
 import de.earthdawn.data.ItemkindType;
@@ -8,7 +7,6 @@ import de.earthdawn.data.ItemkindType;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.JSpinner;
-import javax.swing.JList;
 import javax.swing.JCheckBox;
 import javax.swing.SpinnerNumberModel;
 import javax.swing.JComboBox;
@@ -16,95 +14,106 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
 import javax.swing.border.LineBorder;
+
+import com.sun.java.swing.plaf.gtk.GTKConstants.TextDirection;
+
 import net.miginfocom.swing.MigLayout;
 
-
 public class ThreadItemNodePanel extends AbstractNodePanel<THREADITEMType> {
-	private static final long serialVersionUID = 1L;
-	private JLabel lblName;
+	private static final long serialVersionUID = -1042688290869805755L;
 	private JTextField textFieldName;
-	private JLabel lblWeight;
 	private JSpinner spinnerWeight;
-	private JLabel lblLocation;
 	private JTextField textFieldLocation;
 	private JCheckBox chckbxUsed;
-	private JLabel lblType;
 	private JComboBox comboBoxType;
-	private JLabel lblDescription;
 	private JTextField textFieldDescription;
-	private JLabel lblSpelldefense;
 	private JSpinner spinnerSpellDefense;
-	private JLabel lblMaxThreads;
 	private JSpinner spinnerMaxThreads;
-	private JLabel lblLpGrowth;
 	private JSpinner spinnerLP;
+	private JTextField textFieldBookRef;
+	private JSpinner spinnerDepatterningrate;
+	private JSpinner spinnerBloodDamage;
+	private JSpinner spinnerEdn;
+	private JTextField textFieldEffect;
+	private JSpinner spinnerWeaventhreadrank;
 
 	public ThreadItemNodePanel(THREADITEMType node) {
 		super(node);
-		this.setPreferredSize(new Dimension(710, 65));
 		setBorder(new LineBorder(new Color(0, 0, 0)));
-		setLayout(new MigLayout("", "[24px][128px,grow][27px][86px][34px][47px][40px][86px][49px]", "[23px][]"));
-		
-		lblType = new JLabel("Type");
-		add(lblType, "cell 0 0,alignx left,aligny center");
-		
+		setLayout(new MigLayout("", "[24px][128px,grow][24px][60px:60px:60px][30px][60px:60px:60px]", "[20px:20px:20px][20px:20px:20px][20px:20px:20px][20px:20px:20px][20px:20px:20px][20px:20px:20px]"));
+
+		add(new JLabel("Type"), "cell 0 0,alignx left,aligny center");
 		comboBoxType = new JComboBox(ItemkindType.values());
 		comboBoxType.setFont(new Font("Tahoma", Font.PLAIN, 10));
-		add(comboBoxType, "cell 1 0,alignx left,aligny center");
+		add(comboBoxType, "cell 1 0,growx,aligny center");
 		comboBoxType.setSelectedItem(nodeObject.getKind());
-		
-		lblName = new JLabel("Name");
-		add(lblName, "cell 2 0,alignx left,aligny center");
-		
+
+		add(new JLabel("EDN"), "cell 2 0,alignx left,aligny center");
+		spinnerEdn = new JSpinner(new SpinnerNumberModel(node.getEnchantingdifficultynumber(), 0, 99, 1));
+		add(spinnerEdn, "cell 3 0,alignx left,aligny center");
+
+		add(new JLabel("Weight"), "cell 2 1,alignx left,aligny center");
+		spinnerWeight = new JSpinner(new SpinnerNumberModel(node.getWeight(), 0, 99, 1));
+		add(spinnerWeight, "cell 3 1,alignx left,aligny center");
+
+		add(new JLabel("LP cost growth"), "cell 4 2");
+		spinnerLP = new JSpinner(new SpinnerNumberModel(node.getLpcostgrowth(), 0, 15, 1));
+		add(spinnerLP, "cell 5 2,alignx left,aligny center");
+
+		add(new JLabel("BookRef"), "cell 0 3,alignx left,aligny center");
+		textFieldBookRef = new JTextField();
+		textFieldBookRef.setText(node.getBookref());
+		add(textFieldBookRef, "cell 1 3,growx,aligny center");
+		textFieldBookRef.setColumns(10);
+
+		add(new JLabel("Max Threads"), "cell 4 3");
+		spinnerMaxThreads = new JSpinner(new SpinnerNumberModel(node.getMaxthreads(), 0, 15, 1));
+		add(spinnerMaxThreads, "cell 5 3,alignx left,aligny center");
+
+		add(new JLabel("Spell Defense"), "cell 2 2");
+		spinnerSpellDefense = new JSpinner(new SpinnerNumberModel(node.getSpelldefense(), 0, 99, 1));
+		add(spinnerSpellDefense, "cell 3 2,alignx left,aligny center");
+
+		add(new JLabel("Thread Rank"), "cell 2 3");
+		spinnerWeaventhreadrank = new JSpinner(new SpinnerNumberModel(node.getWeaventhreadrank(), 0, 15, 1));
+		add(spinnerWeaventhreadrank, "cell 3 3,alignx left,aligny center");
+
+		add(new JLabel("Name"), "cell 0 2,alignx left,aligny center");
 		textFieldName = new JTextField();
-		add(textFieldName, "cell 3 0,alignx left,aligny center");
-		textFieldName.setColumns(10);
+		add(textFieldName, "cell 1 2,growx,aligny center");
+		textFieldName.setColumns(12);
 		textFieldName.setText(nodeObject.getName());
-		
-		lblWeight = new JLabel("Weight");
-		add(lblWeight, "cell 4 0,alignx left,aligny center");
-		
-		spinnerWeight = new JSpinner();
-		spinnerWeight.setModel(new SpinnerNumberModel(node.getWeight(), 0, 100, 1));
-		add(spinnerWeight, "cell 5 0,alignx left,aligny center");
-		
-		lblLocation = new JLabel("Location");
-		add(lblLocation, "cell 6 0,alignx left,aligny center");
-		
+
+		add(new JLabel("Location"), "cell 0 1,alignx left,aligny center");
 		textFieldLocation = new JTextField();
-		add(textFieldLocation, "cell 7 0,alignx left,aligny center");
+		add(textFieldLocation, "cell 1 1,growx,aligny center");
 		textFieldLocation.setColumns(10);
 		textFieldLocation.setText(node.getLocation());
-		
+
+		add(new JLabel("Blood Damage"), "cell 4 0,alignx left,aligny center");
+		spinnerBloodDamage = new JSpinner(new SpinnerNumberModel(node.getBlooddamage(), 0, 99, 1));
+		add(spinnerBloodDamage, "cell 5 0,alignx left,aligny center");
+
+		add(new JLabel("Depatterningrate"), "cell 4 1");
+		spinnerDepatterningrate = new JSpinner(new SpinnerNumberModel(node.getDepatterningrate(), 0, 99, 1));
+		add(spinnerDepatterningrate, "cell 5 1,alignx left,aligny center");
+
+		add(new JLabel("Effect"), "cell 0 4");
+		textFieldEffect = new JTextField();
+		add(textFieldEffect, "cell 1 4 4 1,growx,aligny center");
+		textFieldEffect.setColumns(12);
+		textFieldEffect.setText(nodeObject.getEffect());
+
 		chckbxUsed = new JCheckBox("Used");
-		chckbxUsed.setBackground(Color.WHITE);
-		add(chckbxUsed, "cell 8 0,alignx left,aligny top");
+		chckbxUsed.setOpaque(false);
+		add(chckbxUsed, "cell 5 4,alignx right,aligny center");
 		chckbxUsed.setSelected(node.getUsed() == YesnoType.YES);
-		
-		lblDescription = new JLabel("Description");
-		add(lblDescription, "cell 0 1,alignx trailing");
-		
+
+		add(new JLabel("Description"), "cell 0 5");
 		textFieldDescription = new JTextField();
-		add(textFieldDescription, "cell 1 1,growx");
-		textFieldDescription.setColumns(10);
-		
-		lblSpelldefense = new JLabel("Spelldefense");
-		add(lblSpelldefense, "cell 2 1");
-		
-		spinnerSpellDefense = new JSpinner();
-		add(spinnerSpellDefense, "cell 3 1");
-		
-		lblMaxThreads = new JLabel("Max threads");
-		add(lblMaxThreads, "cell 4 1");
-		
-		spinnerMaxThreads = new JSpinner();
-		add(spinnerMaxThreads, "cell 5 1");
-		
-		lblLpGrowth = new JLabel("LP growth");
-		add(lblLpGrowth, "cell 6 1");
-		
-		spinnerLP = new JSpinner();
-		add(spinnerLP, "cell 7 1");
+		add(textFieldDescription, "cell 1 5 5 1,growx,aligny center");
+		textFieldDescription.setColumns(12);
+		textFieldDescription.setText(nodeObject.getDescription());
 	}
 
 	@Override
@@ -113,13 +122,16 @@ public class ThreadItemNodePanel extends AbstractNodePanel<THREADITEMType> {
 		nodeObject.setName(textFieldName.getText());
 		nodeObject.setWeight(((Double) spinnerWeight.getValue()).floatValue());
 		nodeObject.setLocation(textFieldLocation.getText());
-		if(chckbxUsed.isSelected()){
-			nodeObject.setUsed(YesnoType.YES);
-		}
-		else{
-			nodeObject.setUsed(YesnoType.NO);
-		}
-		
+		nodeObject.setBookref(textFieldBookRef.getText());
+		nodeObject.setBlooddamage((Integer) spinnerBloodDamage.getValue());
+		nodeObject.setDepatterningrate((Integer) spinnerDepatterningrate.getValue());
+		nodeObject.setEnchantingdifficultynumber((Integer) spinnerEdn.getValue());
+		if(chckbxUsed.isSelected()) nodeObject.setUsed(YesnoType.YES);
+		else nodeObject.setUsed(YesnoType.NO);
+		nodeObject.setDescription(textFieldDescription.getText());
+		nodeObject.setLpcostgrowth((Integer)spinnerLP.getValue());
+		nodeObject.setMaxthreads((Integer)spinnerMaxThreads.getValue());
+		nodeObject.setSpelldefense((Integer)spinnerSpellDefense.getValue());
+		nodeObject.setWeaventhreadrank((Integer)spinnerWeaventhreadrank.getValue());
 	}
-
 }
