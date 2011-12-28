93e8110f7ce0be3bd95f59379b4e1cdefe308995
Holger <lortas@freenet.de>
2011-12-28 22:29:56 +0000

    Eigene Klasse SkillsTableModel. Diese wird nun auch in der EDLanguages.java verwendet

---
+++ src/de/earthdawn/ui2/SkillsTableModel.java
@@ -0,0 +1,119 @@
+package de.earthdawn.ui2;
+/******************************************************************************\
+Copyright (C) 2010-2011  Holger von Rhein <lortas@freenet.de>
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+\******************************************************************************/
+
+import java.util.List;
+
+import javax.swing.table.AbstractTableModel;
+
+import de.earthdawn.CharacterContainer;
+import de.earthdawn.config.ApplicationProperties;
+import de.earthdawn.data.RANKType;
+import de.earthdawn.data.SKILLType;
+
+public class SkillsTableModel extends AbstractTableModel {
+	private static final long serialVersionUID = 1321139806762229027L;
+	public static final ApplicationProperties PROPERTIES=ApplicationProperties.create();
+	public static final List<String> languageSkillSpeakNames = PROPERTIES.getLanguageSkillSpeakName();
+	public static final List<String> languageSkillReadWriteNames = PROPERTIES.getLanguageSkillReadWriteName();
+	private CharacterContainer character;
+	private boolean languageSkills;
+	private String[] columnNames = {"Name", "Limitation",  "Attribute", "Startrank", "Rank", "Action", "Step", "Dice", "Bookref"};
+
+	public SkillsTableModel(CharacterContainer character, boolean languageSkills) {
+		super();
+		this.character = character;
+		this.languageSkills = languageSkills;
+	}
+
+	public void setCharacter(CharacterContainer character) {
+		this.character = character;
+		fireTableStructureChanged();
+	}
+
+	public CharacterContainer getCharacter() {
+		return character;
+	}
+
+	public int getColumnCount() {
+		return columnNames.length;
+	}
+
+	public int getRowCount() {
+		if( character == null ) return 0;
+		if( languageSkills ) return character.getOnlyLanguageSkills().size();
+		return character.getSkills().size();
+	}
+
+	public String getColumnName(int col) {
+		return columnNames[col];
+	}
+
+	public Object getValueAt(int row, int col) {
+		if( character == null ) return "";
+		// {"Name", "Limitation",  "Attribute", "Rank", "Startrank", "Action", "Step" "Dice"};
+		SKILLType skill;
+		if( languageSkills ) skill = character.getOnlyLanguageSkills().get(row);
+		else skill = character.getSkills().get(row);
+		switch (col) {
+		case 0: return skill.getName();
+		case 1: return skill.getLimitation();
+		case 2: return skill.getAttribute();
+		case 3: return new Integer(skill.getRANK().getStartrank());
+		case 4: return new Integer(skill.getRANK().getRank());
+		case 5: return skill.getAction().value();
+		case 6: return skill.getRANK().getStep();
+		case 7: return skill.getRANK().getDice().value();
+		case 8: return skill.getBookref();
+		default : return new String("Error not defined");
+		}
+	}
+
+	public Class getColumnClass(int c) {
+		return getValueAt(0, c).getClass();
+	}
+
+	public boolean isCellEditable(int row, int col) {
+		if( col == 4 ) return true;
+		if( languageSkills ) return false;
+		SKILLType skill = character.getSkills().get(row);
+		if( languageSkillSpeakNames.contains(skill.getName()) ) return false;
+		if( languageSkillReadWriteNames.contains(skill.getName()) ) return false;
+		if( col==1 ) return true;
+		if( col==3 ) return true;
+		return false;
+	}
+
+	public void setValueAt(Object value, int row, int col) {
+		SKILLType skill;
+		if( languageSkills ) skill = character.getOnlyLanguageSkills().get(row);
+		else skill = character.getSkills().get(row);
+		if( (col==1) && !languageSkills ) skill.setLimitation((String)value);
+		if( (col==3) && !languageSkills ) {
+			int v = (Integer)value;
+			RANKType rank = skill.getRANK();
+			if( (rank.getRank()<v) || (rank.getRank()==rank.getStartrank()) ) {
+				rank.setRank(v);
+			}
+			rank.setStartrank(v);
+		}
+		if(col == 4) skill.getRANK().setRank((Integer) value);
+		character.refesh();
+		fireTableRowsUpdated(row, row);
+	}
+}
