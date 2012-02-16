61d7a8979ba8cc828c1b346019b9b19729d779c7
Holger <lortas@freenet.de>
2012-02-16 18:42:12 +0000

    Items vom Store in den Character nun übertragbar

--- src/de/earthdawn/ui2/EDItemStore.java
+++ src/de/earthdawn/ui2/EDItemStore.java
@@ -1,12 +1,12 @@
 package de.earthdawn.ui2;
 
-import java.awt.BorderLayout;
 import java.awt.Component;
-import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.GraphicsConfiguration;
 import java.awt.HeadlessException;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
@@ -15,16 +15,23 @@ import java.io.IOException;
 
 import javax.imageio.ImageIO;
 import javax.swing.JFrame;
-import javax.swing.JPanel;
+import javax.swing.JMenuItem;
 import javax.swing.JPopupMenu;
 import javax.swing.JScrollPane;
 import javax.swing.JTree;
 import javax.swing.tree.TreePath;
 
+import de.earthdawn.CharacterContainer;
 import de.earthdawn.config.ApplicationProperties;
+import de.earthdawn.data.ARMORType;
+import de.earthdawn.data.EDCHARACTER;
 import de.earthdawn.data.ITEMS;
+import de.earthdawn.data.ITEMType;
+import de.earthdawn.data.MAGICITEMType;
+import de.earthdawn.data.SHIELDType;
+import de.earthdawn.data.THREADITEMType;
+import de.earthdawn.data.WEAPONType;
 import de.earthdawn.ui2.tree.ItemStoreTreeModel;
-import de.earthdawn.ui2.tree.ItemTreeCellEditor;
 import de.earthdawn.ui2.tree.ItemTreeCellRenderer;
 
 public class EDItemStore extends JFrame {
@@ -35,24 +42,25 @@ public class EDItemStore extends JFrame {
 	private BufferedImage backgroundimage = null;
 	private Object currentNode; 
 	private TreePath currentPath;
-	private Component parent;
+	private EDInventory parent;
+	private CharacterContainer character=null;
 
-	public EDItemStore(Component parent) throws HeadlessException {
+	public EDItemStore(EDInventory parent) throws HeadlessException {
 		super("Earthdawn Character Item Store");
 		initialize(parent);
 	}
 
-	public EDItemStore(Component parent, GraphicsConfiguration gc) {
+	public EDItemStore(EDInventory parent, GraphicsConfiguration gc) {
 		super(gc);
 		initialize(parent);
 	}
 
-	public EDItemStore(Component parent, String title) throws HeadlessException {
+	public EDItemStore(EDInventory parent, String title) throws HeadlessException {
 		super(title);
 		initialize(parent);
 	}
 
-	public EDItemStore(Component parent, String title, GraphicsConfiguration gc) {
+	public EDItemStore(EDInventory parent, String title, GraphicsConfiguration gc) {
 		super(title, gc);
 		initialize(parent);
 	}
@@ -72,7 +80,7 @@ public class EDItemStore extends JFrame {
 		if( backgroundimage != null ) g.drawImage(backgroundimage, 0, 0, getWidth(), getHeight(), this);
 	}
 
-	private void initialize(Component parent) {
+	private void initialize(EDInventory parent) {
 		this.parent=parent;
 		tree = new JTree(new ItemStoreTreeModel(items));
 		tree.addMouseListener(new MouseAdapter() {
@@ -92,18 +100,91 @@ public class EDItemStore extends JFrame {
 		scrollPane.setViewportView(tree);
 		scrollPane.getViewport().setOpaque(false);
 		setContentPane(scrollPane);
-		setLocationRelativeTo(parent);
+		setLocationRelativeTo(this.parent);
 		setSize(new Dimension(500,500));
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 	}
 
+	public void setCharacter(CharacterContainer character) {
+		this.character=character;
+	}
+
 	protected void do_tree_mouseReleased(MouseEvent event) {
+		if( character == null ) return;
 		if( event.getButton() < 2 ) return;
 		currentPath = tree.getPathForLocation(event.getX(), event.getY());
 		if( currentPath == null ) return;
 		currentNode = currentPath.getLastPathComponent();
 		if( currentNode == null ) return;
 		JPopupMenu popup = new JPopupMenu();
+
+		if(currentNode instanceof THREADITEMType) {
+			JMenuItem menuitem = new JMenuItem("Add to Character");
+			menuitem.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent arg0) {
+					character.getThreadItem().add((THREADITEMType)currentNode);
+					character.refesh();
+				}
+			});
+			popup.add(menuitem);
+		}
+		if(currentNode instanceof ARMORType) {
+			JMenuItem menuitem = new JMenuItem("Add to Character");
+			menuitem.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent arg0) {
+					character.getProtection().getARMOROrSHIELD().add((ARMORType)currentNode);
+					character.refesh();
+				}
+			});
+			popup.add(menuitem);
+		}
+		if(currentNode instanceof SHIELDType) {
+			JMenuItem menuitem = new JMenuItem("Add to Character");
+			menuitem.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent arg0) {
+					character.getProtection().getARMOROrSHIELD().add((SHIELDType)currentNode);
+					character.refesh();
+				}
+			});
+			popup.add(menuitem);
+		}
+		if(currentNode instanceof WEAPONType) {
+			JMenuItem menuitem = new JMenuItem("Add to Character");
+			menuitem.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent arg0) {
+					character.getWeapons().add((WEAPONType)currentNode);
+					character.refesh();
+				}
+			});
+			popup.add(menuitem);
+		}
+		if(currentNode instanceof MAGICITEMType) {
+			Object parrentNode = currentPath.getParentPath().getLastPathComponent();
+			if( (parrentNode instanceof String) && ((String)parrentNode).equals("Bloodcharms")) {
+				JMenuItem menuitem = new JMenuItem("Add to Character");
+				menuitem.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent arg0) {
+						character.getBloodCharmItem().add((MAGICITEMType)currentNode);
+						character.refesh();
+					}
+				});
+				popup.add(menuitem);
+			}
+		}
+		if(currentNode instanceof ITEMType) {
+			Object parrentNode = currentPath.getParentPath().getLastPathComponent();
+			if( (parrentNode instanceof String) && ((String)parrentNode).equals("Items")) {
+				JMenuItem menuitem = new JMenuItem("Add to Character");
+				menuitem.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent arg0) {
+						character.getItems().add((ITEMType)currentNode);
+						character.refesh();
+					}
+				});
+				popup.add(menuitem);
+			}
+		}
+
 		tree.add(popup);
 		popup.show(tree, event.getX(), event.getY());
 	}
