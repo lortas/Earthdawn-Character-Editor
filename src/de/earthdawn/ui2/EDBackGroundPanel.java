b984a7ba9e535e53f50ca344dcd6bddc4c22cbb7
Holger <lortas@freenet.de>
2012-03-04 22:16:46 +0000

    Hintergrundanpassungen

--- src/de/earthdawn/ui2/EDBackGroundPanel.java
+++ src/de/earthdawn/ui2/EDBackGroundPanel.java
@@ -6,21 +6,17 @@ import java.awt.Image;
 import javax.swing.JPanel;
 
 public class EDBackGroundPanel extends JPanel {
+	private static final long serialVersionUID = 7069058498214662662L;
 	private Image image;
 	
-	public EDBackGroundPanel(Image ing)
-	{
+	public EDBackGroundPanel(Image img) {
 		setOpaque(false);
-		image = ing;
-		
+		image = img;
 	}
 
 	@Override
-	public void paintComponent(Graphics g)
-	{
-		if(image != null)
-			g.drawImage(image, 0, 0, getWidth(), getHeight(), this);
+	public void paintComponent(Graphics g) {
+		if( image != null ) g.drawImage(image, 0, 0, getWidth(), getHeight(), this);
 		super.paintComponent(g);
 	}
-
 }
