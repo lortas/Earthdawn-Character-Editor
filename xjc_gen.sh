30c16ebd5cdfea08145779d5ec3d6e5b69f05bb2
lortas <lortas@freenet.de>
2015-04-29 00:52:37 +0200

    xjc always witn english comments

--- xjc_gen.sh
+++ xjc_gen.sh
@@ -1,3 +1,4 @@
 #! /bin/sh
+export LANG=C
 xjc -p de.earthdawn.data -b config/bindings.xml -d src config/earthdawn*.xsd config/ece*xsd  config/charsheettemplate.xsd
 sed -i -e "s,// Generated on: .*,// Generated on: see version control commit date," src/de/earthdawn/data/*
