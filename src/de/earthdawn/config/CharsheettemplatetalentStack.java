e570a4f6bef130d4af5d23132aa927a3f27eb493
lortas <lortas@freenet.de>
2015-05-02 13:31:22 +0200

    create subclass type within CharsheettemplatetalentStack

--- src/de/earthdawn/config/CharsheettemplatetalentStack.java
+++ src/de/earthdawn/config/CharsheettemplatetalentStack.java
@@ -1,37 +1,31 @@
 package de.earthdawn.config;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Iterator;
+import java.util.LinkedList;
 
 import de.earthdawn.data.CharsheettemplatetalentType;
 
 public class CharsheettemplatetalentStack {
-	private List<CharsheettemplatetalentType> stack = new ArrayList<CharsheettemplatetalentType>();
+	public static class type {
+		public boolean isDiscipline=false;
+		public boolean isOther=false;
+	}
+	private LinkedList<CharsheettemplatetalentType> stack = new LinkedList<CharsheettemplatetalentType>();
 
 	public void push(CharsheettemplatetalentType item) {
 		stack.add(item);
 	}
 
-	public CharsheettemplatetalentType pull(int discipline) {
-		int i=nextIndex(discipline);
-		if( i<0 ) return null;
-		CharsheettemplatetalentType e = stack.get(i);
-		stack.remove(i);
-		return e;
-	}
-
-	public boolean hasNext(int discipline) {
-		if( nextIndex(discipline) < 0 ) return false;
-		return true;
-	}
-
-	public int nextIndex(int discipline) {
-		for( int i=0; i<stack.size(); i++ ) {
-			CharsheettemplatetalentType e = stack.get(i);
-			if( (discipline==0) || ((discipline==1) && e.isDiscipline()) || ((discipline==-1) && !e.isDiscipline()) ) {
-				return i;
+	public CharsheettemplatetalentType pull(type fieldkind) {
+		Iterator<CharsheettemplatetalentType> itr = stack.iterator();
+		while( itr.hasNext() ) {
+			CharsheettemplatetalentType e = itr.next();
+			if( ( fieldkind.isDiscipline && e.isDiscipline() ) || 
+				( fieldkind.isOther && e.isOther() ) ) {
+				itr.remove();
+				return e;
 			}
 		}
-		return -1;
+		return null;
 	}
 }
