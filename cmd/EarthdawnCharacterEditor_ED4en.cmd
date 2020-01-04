27a1aed33fc2e958dc7ef756f8fb4466eedd73bd
lortas <lortas@freenet.de>
2020-01-04 14:52:46 +0100

    build two jars one legacy and one current. the start scripts check for java version and run the jar fiting best.

--- cmd/EarthdawnCharacterEditor_ED4en.cmd
+++ cmd/EarthdawnCharacterEditor_ED4en.cmd
@@ -1,1 +1,12 @@
-java -jar EarthdawnCharacterEditor.jar --rulesetversion ED4 --language en
+SET JAR=EarthdawnCharacterEditor.jar
+
+FOR /f tokens^=2-5^ delims^=.-_^" %%j IN ('java -version 2^>^&1') DO SET "JV=%%j%%k%%l%%m"
+IF %ERRORLEVEL% NEQ 0 (
+	echo "no java found"
+	EXIT 1
+)
+IF %JV% LSS 110000 (
+	SET JAR=EarthdawnCharacterEditor_java8.jar
+)
+
+java -jar %JAR% --rulesetversion ED4 --language en
--- cmd/EarthdawnCharacterEditor_ED4en.sh
+++ cmd/EarthdawnCharacterEditor_ED4en.sh
@@ -1,2 +1,24 @@
 #!/bin/sh
-java -jar EarthdawnCharacterEditor.jar --rulesetversion ED4 --language en
+
+JAR=EarthdawnCharacterEditor.jar
+
+JV=`java --version`
+if test $? -ne 0
+then
+	echo "no java found"
+	exit 1
+fi
+
+JV=`echo $JV | head -1 | sed -e "s/^[^ ]* \+//" -e "s/\..*//"`
+if test -z "$JV"
+then
+	echo "can not identifiy java version"
+	exit 2
+fi
+
+if test "$JV" -lt 11
+then
+	JAR=EarthdawnCharacterEditor_java8.jar
+fi
+
+java -jar "${JAR}" --rulesetversion ED4 --language en
