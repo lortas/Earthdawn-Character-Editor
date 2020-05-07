93e94b5652c6547f8fe5c7de97b7c6ee7ea31a43
lortas <lortas@freenet.de>
2020-05-07 08:54:08 +0200

    update java classes which are generated by xml schema definitions

--- src/de/earthdawn/data/ObjectFactory.java
+++ src/de/earthdawn/data/ObjectFactory.java
@@ -750,6 +750,22 @@ public class ObjectFactory {
     }
 
     /**
+     * Create an instance of {@link QUESTOR }
+     *
+     */
+    public QUESTOR createQUESTOR() {
+        return new QUESTOR();
+    }
+
+    /**
+     * Create an instance of {@link QUESTORTIERType }
+     *
+     */
+    public QUESTORTIERType createQUESTORTIERType() {
+        return new QUESTORTIERType();
+    }
+
+    /**
      * Create an instance of {@link DISCIPLINESPELLType }
      *
      */
@@ -763,6 +779,14 @@ public class ObjectFactory {
      */
     public FOREIGNTALENTSType createFOREIGNTALENTSType() {
         return new FOREIGNTALENTSType();
+    }
+
+    /**
+     * Create an instance of {@link QUESTORRANKType }
+     *
+     */
+    public QUESTORRANKType createQUESTORRANKType() {
+        return new QUESTORRANKType();
     }
 
     /**
@@ -1115,6 +1139,14 @@ public class ObjectFactory {
      */
     public TRANSLATIONType createTRANSLATIONType() {
         return new TRANSLATIONType();
+    }
+
+    /**
+     * Create an instance of {@link NAMETIERKINDType }
+     *
+     */
+    public NAMETIERKINDType createNAMETIERKINDType() {
+        return new NAMETIERKINDType();
     }
 
     /**
