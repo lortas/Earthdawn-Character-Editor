c70ab028adab2c774b0108d26fbd38eb2d81c37e
lortas <lortas@freenet.de>
2020-10-04 15:50:03 +0200

    windows batch startup fixed
    Errors will now raise as popup and no longer shown on console
    Test for first parameter instead of second for java version

--- cmd/EarthdawnCharacterEditor_ED3en.cmd
+++ cmd/EarthdawnCharacterEditor_ED3en.cmd
@@ -2,22 +2,27 @@
 
 java -version > nul 2>&1
 IF %ERRORLEVEL% NEQ 0 (
-	echo "no java found"
+	msg * "Can not find any java to run."
 	goto :end
 )
 
 for /f tokens^=2-5^ delims^=.-_^" %%j in ('java -version 2^>^&1') do (
-	SET /a "JV1=%%j"
-	SET /a "JV2=%%k"
-	SET /a "JV3=%%l"
-	SET /a "JV4=%%m"
+	SET "JV1=%%j"
+	SET "JV2=%%k"
+	SET "JV3=%%l"
+	SET "JV4=%%m"
 	goto :decide
 )
 
 :decide
-set JAR=EarthdawnCharacterEditor.jar
-IF %JV2% LSS 11 (
-	set JAR=EarthdawnCharacterEditor_java8.jar
+IF "%JV1%" == "" (
+	msg * "Can not identifiy java version."
+	goto :end
+)
+SET /a "JV1=%JV1%"
+SET JAR=EarthdawnCharacterEditor.jar
+IF %JV1% LSS 11 (
+	SET JAR=EarthdawnCharacterEditor_java8.jar
 )
 java -jar %JAR% --rulesetversion ED3 --language en
 
